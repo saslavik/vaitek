6b13c1c57c543976192c310b1aa584287b013817
saslavik <saslavik@gmail.com>
2021-04-04 16:59:27 +0300

    mobile finder

--- js/script.js
+++ js/script.js
@@ -13,11 +13,16 @@ $(document).ready(function() {
   $('#main').click(function(event) {
     $('.find_input').removeClass('find_active')
   })
+  $('.close_finder').click(function(event) {
+    $('.find_input').removeClass('find_active')
+  })
   $('.burger_menu').click(function(event) {
     $('.header_content').toggleClass('header_content_active')
+    $('body').toggleClass('body_lock')
   })
   $('.close_burger').click(function(event) {
     $('.header_content').removeClass('header_content_active')
+    $('body').removeClass('body_lock')
   })
 })
 
